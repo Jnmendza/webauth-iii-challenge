6917b4e5c83882d297e323f3de6b79f01144ab16
Jnmendza <jonatmendoza@gmail.com>
2019-11-13 16:36:28 -0800

    MVP

--- database/seeds/01_users.js
+++ database/seeds/01_users.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex) {
     return knex('users').insert([
-        {username: 'Username1', password: 'pass1'},
-        {username: 'Username2', password: 'pass2'},
-        {username: 'Username3', password: 'pass3'}
+        {username: 'Username1', password: 'pass1', department: 'Dev'},
+        {username: 'Username2', password: 'pass2', department: 'IT'},
+        {username: 'Username3', password: 'pass3', department: 'Graphics'}
     ])
 }
